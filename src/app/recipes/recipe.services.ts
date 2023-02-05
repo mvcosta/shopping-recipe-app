86c1abd4811e3e3040e7e653d93d13e1bdabd7fb
mvcosta <mvcoosta@gmail.com>
2023-02-04 23:54:59 -0400

    Implemented recipes ingredient list

--- src/app/recipes/recipe.services.ts
+++ src/app/recipes/recipe.services.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -6,14 +7,20 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is a simple test',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg'
+      'Schnitzel',
+      'Awesome',
+      'https://static.dw.com/image/19338867_6.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      'Another Test Recipe',
-      'This is another test',
-      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/olive-garden-ziti-al-forno-recipe-2-1640031063.jpeg'
+      'Burguer',
+      'Delicious',
+      'https://comeonburger.com.br/wp-content/uploads/2019/12/xburger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+        new Ingredient('Cheese', 1),
+      ]
     ),
   ];
 
